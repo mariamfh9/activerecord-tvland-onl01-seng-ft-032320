a75e83eeb9d7d5d13685a160983ad5ba16e5da76
Mariam Haider <mfh.haider@gmail.com>
2020-04-28 19:13:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+
+  def actors_list
+    self.characters.map do |character|
+        "#{character.actor.first_name} #{character.actor.last_name}"
+    end
+  end
 end
